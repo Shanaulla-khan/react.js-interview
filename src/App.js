8a5a4f940e35e4946bf017db73da8568c528c608
pathanshanaullakhan <101649780+pathanshanaullakhan@users.noreply.github.com>
2022-12-05 11:06:45 +0530

    interview question

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import {Counter} from './component/useState/Counter';
 import {TodoList} from './component/useState/TodoList';
+import {Counter2} from './component/useEffect/Counter2';
 
 function App() {
   return (
     <div className="App">
       <Counter />
       <TodoList />
+      <Counter2 />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/component/useEffect/Counter2.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+
+export const Counter2=()=>{
+    const [count, setCount]=useState(0);
+    const [data, setData]=useState('');
+
+    useEffect(()=>{
+     if(count % 2===0){
+        setData('blue')
+     }
+     else if(count % 2===1){
+        setData('orange')
+     }
+    },[count])
+    return(
+        <div>
+            {/* //Print the Even value to change the color blue// */}
+            {/* //print the odd value to change the color orange// */}
+            <h1>Counter 2.O</h1>
+            <h1 style={{color: data}}>{count}</h1>
+            <button onClick={()=>setCount(count+1)}>Increment</button>
+            <button onClick={()=>setCount(count-1)}>Decrement</button>
+            <button onClick={()=>setCount(0)}>Reset</button>
+        </div>
+    )
+}
